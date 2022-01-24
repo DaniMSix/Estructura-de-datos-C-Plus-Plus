d39c5909edc8e3cbe1d602d37ff02930108a342e
DaniMSix <dani.morales.sixto@gmail.com>
2022-01-23 23:23:45 -0600

    Función

--- Recursividad/SerieFibonacci.cpp
+++ Recursividad/SerieFibonacci.cpp
@@ -5,6 +5,22 @@ Programa que calcula el enésimo número de Fibonacci
 #include <iostream>
 using namespace std;
 
-int fiboncci(int num){
-    
+int fibonacci(int num){
+    // Caso base
+    if (num == 0){
+        return 0;
+    } else if (num == 1){
+        return 1;
+    } else {
+        return fibonacci(num-1) + fibonacci(num-2);
+    }
+}
+
+int main(){
+    int num;
+
+    cout<<"Ingrese el número del término que desea ver\n";
+    cin>>num;
+
+    cout<<fibonacci(num);
 }
